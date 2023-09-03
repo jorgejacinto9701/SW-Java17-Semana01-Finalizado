78c1a752d02ab15bf760d15abb6f260bfa9329c8
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-09-02 19:03:31 -0500

    ajustes

--- src/main/java/com/example/demo/socket/archivos/Server.java
+++ src/main/java/com/example/demo/socket/archivos/Server.java
@@ -1,12 +1,9 @@
 package com.example.demo.socket.archivos;
 
-import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
